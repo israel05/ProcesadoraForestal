e860c94eb4752fbda74d8cc4e7758d50cda59281
GRANDE-I7\idela <i.delamo@gmail.com>
2020-09-12 12:57:37 +0200

    Hoja con movimiento limitado

--- ProcesadoraBase/Assets/Scripts/Usar_Sierra.cs
+++ ProcesadoraBase/Assets/Scripts/Usar_Sierra.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -13,6 +14,26 @@ public class Usar_Sierra : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            print("USANDO SIERRA Dentro");
+            MoverHojaDentro();
+
+        }
+        if (Input.GetKeyDown(KeyCode.Q))
+        {
+            print("USANDO SIERRA Fuera");
+            MoverHojaFuera();
+
+        }
+    }
+
+    private void MoverHojaDentro()
+    {
+        transform.Rotate(new Vector3(0f, 10f, 0f));
+    }
+    private void MoverHojaFuera()
+    {
+        transform.Rotate(new Vector3(0f, -10f, 0f));
     }
 }
