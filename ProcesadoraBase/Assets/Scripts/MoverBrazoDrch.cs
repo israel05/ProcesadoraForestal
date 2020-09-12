79bb1c758a52e506a26bcd65e9bfb63bc2dd311c
GRANDE-I7\idela <i.delamo@gmail.com>
2020-09-12 16:19:22 +0200

    primeros agarres

---
+++ ProcesadoraBase/Assets/Scripts/MoverBrazoDrch.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoverBrazoDrch : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.W))
+        {
+            print("CerrandoBrazoIzq");
+            CerrarBrazoDRC();
+
+        }
+        if (Input.GetKeyDown(KeyCode.S))
+        {
+            print("AbriendoBrazoIzq");
+            AbrirBrazoDRC();
+
+        }
+    }
+
+    private void CerrarBrazoDRC()
+    {
+        transform.Rotate(new Vector3(0f, 0f, -10f));
+    }
+    private void AbrirBrazoDRC()
+    {
+        transform.Rotate(new Vector3(0f, 0f, 10f));
+    }
+}
---
+++ ProcesadoraBase/Assets/Scripts/MoverBrazoIzquierd.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoverBrazoIzquierd : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            print("CerrandoBrazoIzq");
+            CerrarBrazoIZQ();
+
+        }
+        if (Input.GetKeyDown(KeyCode.D))
+        {
+            print("AbriendoBrazoIzq");
+            AbrirBrazoIZQ();
+
+        }
+    }
+
+    private void CerrarBrazoIZQ()
+    {
+        transform.Rotate(new Vector3(0f, 0f, 10f));
+    }
+    private void AbrirBrazoIZQ()
+    {
+        transform.Rotate(new Vector3(0f, 0f, -10f));
+    }
+}
